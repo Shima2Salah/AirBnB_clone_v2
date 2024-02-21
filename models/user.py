195f5bdb59f46c815e12732ac5fcede34fa3013d
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-21 22:56:10 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -15,10 +15,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all, delete, delete-orphan",
-                              backref="user")
-        reviews = relationship("Review", cascade="all, delete, delete-orphan",
-                               backref="user")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ''
         password = ''
