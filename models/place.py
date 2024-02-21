f7b259441f490c9c48ad5001c739132b8d22bd8f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-22 01:07:35 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -9,10 +9,10 @@ from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                    Column(place_id, String(60),
+                    Column('place_id', String(60),
                            ForeignKey("places.id"), primary_key=True,
                            nullable=False),
-                    Column(amenity_id, String(60),
+                    Column('amenity_id', String(60),
                            ForeignKey("amenities.id"),
                            primary_key=True, nullable=False))
 
