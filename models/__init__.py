2577d20daec8acdebc0735d2a3cdb685dd87e09c
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-21 21:56:29 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,9 @@ from os import getenv
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
+storage_t = getenv('HBNB_TYPE_STORAGE')
 
-if storage_type == 'db':
+if storage_t == 'db':
     storage = DBStorage()
 else:
     storage = FileStorage()
