df28a75916a944df831a13feb0de3f6d2470ad47
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-02-21 14:09:16 -0800

    Airbnb

--- models/review.py
+++ models/review.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python
-""" holds class Review"""
-import models
+#!/usr/bin/python3
+""" Review Module for HBNB project """
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
+import models
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
-    if models.storage_t == 'db':
+    """class review attributes"""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
     else:
         place_id = ""
@@ -20,5 +20,5 @@ class Review(BaseModel, Base):
         text = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Review"""
+        """func to initialize Reviews"""
         super().__init__(*args, **kwargs)
