0f867e830c49d98fd1f377bfd2b50e2c9e3f05d1
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-22 00:46:55 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-"""class for all amenities data"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+#!/usr/bin/python
+""" holds class Amenity"""
 import models
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ The Amenity class, contains name """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    """Representation of Amenity """
+    if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """func to initialize amenity class"""
+        """initializes Amenity"""
         super().__init__(*args, **kwargs)
