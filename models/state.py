2e2c04c294fb37667c569c46e6ba8a5a6c1f738f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-21 20:22:52 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
+from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
