8c2e8f074c215aa7b5fe969ec64977af9f519d0f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-21 21:59:19 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Module to manage file storage for hbnb clone"""
+
 from os import getenv
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
-storage_t = getenv('HBNB_TYPE_STORAGE')
 
-if storage_t == 'db':
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
