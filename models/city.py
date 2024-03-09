d6e68437591839a17ef5a102249d74c073292a10
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-09 20:48:21 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 import models
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
