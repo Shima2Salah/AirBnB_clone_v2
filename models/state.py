445dd5bbf3e4312be22612798c999ce29877a5b7
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-09 20:57:27 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.city import City
-from models.base_model import BaseModel, Base
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class State(BaseModel, Base):
