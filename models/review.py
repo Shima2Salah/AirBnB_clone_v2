335246bf1ae4e59ed3d95aec893e25a0919d609e
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-09 20:56:43 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Review Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 import models
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
