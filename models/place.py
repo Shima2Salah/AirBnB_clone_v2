14f54cea2bdc8379b2551a5e7e325e7e351abf56
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-29 23:04:37 +0200

    airbnb

--- models/place.py
+++ models/place.py
@@ -10,19 +10,23 @@ from sqlalchemy.orm import relationship
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                    Column('place_id', String(60),
-                           ForeignKey("places.id"), primary_key=True,
-                           nullable=False),
-                    Column('amenity_id', String(60),
-                           ForeignKey("amenities.id"),
-                           primary_key=True, nullable=False))
+                           Column('place_id', String(60),
+                                   ForeignKey("places.id"),
+                                   primary_key=True,
+                                   nullable=False),
+                           Column('amenity_id',
+                                   String(60),
+                                   ForeignKey("amenities.id"),
+                                   primary_key=True, 
+                                   nullable=False))
 
 
 class Place(BaseModel, Base):
     """class place attributes"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'places'
-        city_id = Column(String(60), collation='latin1_swedish_ci', ForeignKey("cities.id"), nullable=False)
+        city_id = Column(String(60), collation='latin1_swedish_ci',
+                         ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
