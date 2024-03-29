26df7dcfa7509c2c733cf8d522c8cafec539b837
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-29 20:50:36 +0200

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,13 +4,13 @@ Contains the FileStorage class
 """
 
 import json
-from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+from models.amenity import Amenity
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
