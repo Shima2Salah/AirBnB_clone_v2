2734ec467b306e48e289ff782d5a27125526cb5f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-30 14:17:48 +0200

    airbnb

--- models/place.py
+++ models/place.py
@@ -10,12 +10,12 @@ from sqlalchemy.orm import relationship
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
+                          Column('place_id', String(60, collation='latin1_swedish_ci'),
                                  ForeignKey("places.id"),
                                  primary_key=True,
                                  nullable=False),
                           Column('amenity_id',
-                                 String(60),
+                                 String(60, collation='latin1_swedish_ci'),
                                  ForeignKey("amenities.id"),
                                  primary_key=True,
                                  nullable=False))
