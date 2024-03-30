22724bd222ff6049cc8d09001a5a80968b736303
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-30 13:39:29 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" Starts a Flask web application. """
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
-from models.city import City
-from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states():
-    """Render template with states
-    """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda x: x.name)
-    return render_template('9-states.html', states=states)
-
+@app.route('/states', strict_slashes=False, defaults={'id': None})
 @app.route('/states/<id>', strict_slashes=False)
-def state(id):
-    """Render template with state id
-    """
-    state = storage.get(State, id)
-    if state:
-        cities = state.cities
-        cities = sorted(cities, key=lambda x: x.name)
-        return render_template('9-states.html', state=state, cities=cities)
-    else:
-        return render_template('9-states.html', state=None), 404
+def states_list(id):
+    return render_template('9-states.html', states=storage.all("State"),
+                           id=id)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown_app(Error):
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_states():
+    return render_template('8-cities_by_states.html',
+                           states=storage.all("State"))
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', 5000)
