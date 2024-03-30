fdee36f0bc2f2dd63b95d2d92100cf00dd35432d
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-30 14:13:38 +0200

    airbnb

--- models/city.py
+++ models/city.py
@@ -13,7 +13,8 @@ class City(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"),
+        state_id = Column(String(60, collation='latin1_swedish_ci'),
+                          ForeignKey("states.id"),
                           nullable=False)
         places = relationship("Place", cascade="all, delete, delete-orphan",
                                 backref="cities")
